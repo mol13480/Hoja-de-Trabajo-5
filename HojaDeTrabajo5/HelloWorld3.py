0c6596ceac7d35341da68756e11b41ecc2cb7f24
Irene <Irene@Irene-PC>
2014-08-22 08:16:45 -0600

    fjkds

---
+++ HojaDeTrabajo5/HelloWorld3.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Irene"
+__date__ ="$22/08/2014 08:15:27 AM$"
+
+if __name__ == "__main__":
+    print "Hello World"
